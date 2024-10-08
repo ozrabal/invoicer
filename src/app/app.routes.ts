e4f0eca08142abebecb6b18824a3b582155e7a90
Piotr Łepkowski <ozrabal@gmail.com>
2024-10-08 22:00:29 +0200

    feat: add account guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginPageComponent } from './pages/login/login-page.component';
 import { authGuard } from './core/auth/auth.guard';
+import { accountGuard } from './core/auth/account.guard';
 
 export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
   {
     path: '',
     component: HomeComponent,
@@ -14,5 +16,6 @@ export const routes: Routes = [
     path: 'login',
     component: LoginPageComponent,
     title: 'Login',
+    canActivate: [accountGuard]
   },
 ];
